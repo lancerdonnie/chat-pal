18c1c1c441938eddeff7a87772674c5949ad4c73
lancerdonnie <jideadedejifirst@gmail.com>
2020-02-13 21:22:39 +0100

    deployment changes

--- server.js
+++ server.js
@@ -98,12 +98,10 @@ io.on('connection', socket => {
   });
 });
 
-if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
-}
 
 const PORT = process.env.PORT || 5000;
 
