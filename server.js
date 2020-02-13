b7fc5db8e2f59ce2dccdc33918cbcee7e067688e
lancerdonnie <jideadedejifirst@gmail.com>
2020-02-13 22:10:16 +0100

    fixed colors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(http);
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 const uuidv4 = require('uuid/v4');
 const path = require('path');
 
